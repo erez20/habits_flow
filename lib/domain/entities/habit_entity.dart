087b197e7c626bec42c6d5bf5eedec9b9294b578
Erez Pickman <erez20@gmail.com>
2026-01-11 17:55:04 +0200

    habit entity

---
+++ lib/domain/entities/habit_entity.dart
@@ -0,0 +1,13 @@
+class HabitEntity {
+  final String id;
+  final String name;
+  final int weight;
+  final int completionCount;
+
+  HabitEntity({
+    required this.id,
+    required this.name,
+    required this.weight,
+    required this.completionCount,
+  });
+}
