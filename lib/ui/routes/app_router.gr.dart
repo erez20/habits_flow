b13099665d9ab955821de8869172f6ab2fc44eaa
Erez <erez20@gmail.com>
2026-02-18 11:29:50 +0200

    rename app_bar_builder

---
+++ lib/ui/routes/app_router.gr.dart
@@ -0,0 +1,30 @@
+// dart format width=80
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// AutoRouterGenerator
+// **************************************************************************
+
+// ignore_for_file: type=lint
+// coverage:ignore-file
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:auto_route/auto_route.dart' as _i2;
+import 'package:habits_flow/ui/screens/active_habits/active_habits_screen/active_habits_screen_provider.dart'
+    as _i1;
+
+/// generated route for
+/// [_i1.ActiveHabitsScreenProvider]
+class ActiveHabitsRouteProvider extends _i2.PageRouteInfo<void> {
+  const ActiveHabitsRouteProvider({List<_i2.PageRouteInfo>? children})
+    : super(ActiveHabitsRouteProvider.name, initialChildren: children);
+
+  static const String name = 'ActiveHabitsRouteProvider';
+
+  static _i2.PageInfo page = _i2.PageInfo(
+    name,
+    builder: (data) {
+      return const _i1.ActiveHabitsScreenProvider();
+    },
+  );
+}
