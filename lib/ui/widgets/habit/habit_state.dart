c13b3bde3acfdc8584e88865aff678ca7a41c654
Erez <erez20@gmail.com>
2026-03-21 12:01:11 +0200

    remove showtool tip

--- lib/ui/widgets/habit/habit_state.dart
+++ lib/ui/widgets/habit/habit_state.dart
@@ -3,26 +3,27 @@ import 'package:habits_flow/domain/entities/habit_entity.dart';
 
 class HabitState extends Equatable {
   final HabitEntity habit;
-  final bool showTooltip;
+
   final bool isSelected;
 
-
-  const HabitState({required this.habit, required this.showTooltip, required this.isSelected,});
+  const HabitState({
+    required this.habit,
+    required this.isSelected,
+  });
 
   @override
-  List<Object?> get props => [habit, showTooltip, isSelected];
+  List<Object?> get props => [habit, isSelected];
 
   factory HabitState.init({required HabitEntity habit}) =>
-      HabitState(habit: habit, showTooltip: false, isSelected: false);
+      HabitState(habit: habit, isSelected: false);
 
   HabitState copyWith({
     HabitEntity? habit,
-    bool? showTooltip,
+
     bool? isSelected,
   }) {
     return HabitState(
       habit: habit ?? this.habit,
-      showTooltip: showTooltip ?? this.showTooltip,
       isSelected: isSelected ?? this.isSelected,
     );
   }
